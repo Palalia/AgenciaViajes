27df6b3f9ed6b210d483979a21dab8d26b472a57
El_pala <tonio.palalia@gmail.com>
2023-01-14 15:02:35 -0600

    pop card carrousel en proceso

--- src/components/DestinosSection.vue
+++ src/components/DestinosSection.vue
@@ -1,242 +1,212 @@
-<template >
-<v-app>
-  <v-sheet
-    class="mx-auto"
-    elevation="1500"
-    max-width="1500"
-    max-height="1500"
-  >
-    <v-slide-group
-      v-model="model"
-      class="pa-4"
-      mandatory
-      show-arrows
-    >
-      <v-slide-item>
-      <v-card
-        class="mx-auto"
-        max-width="500"
-      >
-    <v-img
-      src="https://one.cdnmega.com/images/destinos/promos/moriente/magia-turca-jerusalen-790x1042_636ab52b89596.webp?width=290"
-      width="500"
-      height="450"
-    ></v-img>
-
-    <v-card-title>
-      Turquia y Jerusalen
-    </v-card-title>
-
-    <v-card-subtitle>
-      10 dias 8 noches
-    </v-card-subtitle>
-
-    <v-card-actions>
-      
-      <v-btn
-        color="orange lighten-2"
-        text>
-        Informacion
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-    
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-        <v-card-text>
-          <p>DÍA 01 MÉXICO ✈ ESTAMBUL</p>
-          <p>DÍA 02 ESTAMBUL</p>
-          <p>DÍA 03 ESTAMBUL</p>
-          <p>DÍA 04 ESTAMBUL </p>
-          <p>DÍA 05 ESTAMBUL 🚌 ANKARA 🚌 CAPADOCIA</p>
-          <p>DÍA 06 CAPADOCIA</p>
-          <p>DÍA 07 CAPADOCIA</p>
-          <p>DÍA 08 CAPADOCIA 🚌 PAMUKKALE</p>
-          <p>DÍA 09 PAMUKKALE 🚌 EFESO 🚌 KUSADASI*</p>
-          <p>DÍA 10 KUSADASI*</p>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-  </v-slide-item>
-<!--//////////////////////-->
-<v-slide-item>
-      <v-card
-        class="mx-auto"
-        max-width="500"
-      >
-    <v-img
-      src="https://one.cdnmega.com/images/destinos/promos/europa/gran-tour-de-europa-790x1042_638559b34ddef.webp?width=290"
-      width="500"
-      height="450"
-    ></v-img>
-    <v-card-title>
-      Europa
-    </v-card-title>
-    <v-card-subtitle>
-      19 dias 17 noches
-    </v-card-subtitle>
-    <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-      Informacion
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click="see = !see"
-      >
-        <v-icon>{{ see ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-    <v-expand-transition>
-      <div v-show="see">
-        <v-divider></v-divider>
-        <v-card-text>
-          <p>DÍA 01 MÉXICO – LONDRES</p>
-          <p>DÍA 02 LONDRES</p>
-          <p>DÍA 04 LONDRES – PARÍS </p>
-          <p>DÍA 05 PARÍS</p>
-          <p>DÍA 07 PARÍS – LUXEMBURGO – FRANKFURT </p>
-          <p>DÍA 08 FRANKFURT – HEIDELBERG – ZÚRICH </p>
-          <p>DÍA 09 ZÚRICH – LUCERNA – VADUZ – INNSBRUCK </p>
-          <p>DÍA 10 INNSBRUCK – PADUA – VENECIA </p>
-          <p>DÍA 11 VENECIA – ROMA </p>
-          <p>DÍA 14 ROMA – FLORENCIA </p>
-          <p>DÍA 15 FLORENCIA – PISA – NIZA </p>
-          <p>DÍA 16 NIZA – BARCELONA</p>
-          <p>DÍA 17 BARCELONA – ZARAGOZA – MADRID  </p>
-          <p>DÍA 18 MADRID  </p>
-          <p>DÍA 19 MADRID – MÉXICO</p>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-  </v-slide-item>
-  <!--/////////////////////////////-->
-  <v-slide-item>
-      <v-card
-        class="mx-auto"
-        max-width="500"
-      >
-    <v-img
-      src="https://one.cdnmega.com/images/viajes/covers/escapate-a-chetumal-1024x575_6320bfe1dfa25.webp"
-      width="575"
-      height="450"
-    ></v-img>
-
-    <v-card-title>
-      Escapate a Chetumal
-    </v-card-title>
-
-    <v-card-subtitle>
-      4 dias / 3 noches
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-      Informacion
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        icon
-        @click="show1 = !show1"
-      >
-        <v-icon>{{ show1 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-    
-    <v-expand-transition>
-      <div v-show="show1">
-        <v-divider></v-divider>
-        <v-card-text>
-          <p>DÍA 01 MÉXICO – CHETUMAL</p>
-          <p>DÍA 02 - 03 CHETUMAL </p>
-          <p>DÍA 04 CHETUMAL – MÉXICO</p>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-  </v-slide-item>
-    <!--/////////////////////////////-->
-    <v-slide-item>
-      <v-card
-        class="mx-auto"
-        max-width="500"
-      >
-    <v-img
-      src="https://one.cdnmega.com/images/destinos/promos/asia/turquia-y-corea-790x1042_637fb0470b0b7.webp?width=290"
-      width="500"
-      height="500"
-    ></v-img>
-
-    <v-card-title>
-      Turquia y Chetumal
-    </v-card-title>
-
-    <v-card-subtitle>
-      15 dias / 10 noches
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-      Informacion
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        icon
-        @click="show2 = !show2"
-      >
-        <v-icon>{{ show2 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-    
-    <v-expand-transition>
-      <div v-show="show2">
-        <v-divider></v-divider>
-        <v-card-text>
-          <p>DÍA 01 MÉXICO – CHETUMAL</p>
-          <p>DÍA 02 -</p>
-          <p>03 CHETUMAL </p>
-          <p>DÍA 04 CHETUMAL – MÉXICO</p>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-  </v-slide-item>      
-</v-slide-group>
-  </v-sheet>
-  </v-app>
+<template>
+  <div >
+     <v-app >
+        <v-content class="mt-15 pt-15">
+           <v-container>
+              <v-sheet
+                 max-width="1500"
+                 max-height="1500"
+                 >
+                 <v-slide-group>
+                    <v-slide-item class="ma-5">
+                       <v-card
+                          class="mx-auto"
+                          max-width="500"
+                          >
+                          <v-img
+                             src="https://one.cdnmega.com/images/destinos/promos/moriente/magia-turca-jerusalen-790x1042_636ab52b89596.webp?width=290"
+                             width="500"
+                             height="450"
+                             ></v-img>
+                          <v-card-title>
+                             Turquia y Jerusalen
+                          </v-card-title>
+                          <v-card-subtitle>
+                             10 dias 8 noches
+                          </v-card-subtitle>
+                          <v-card-actions>
+                             <PopCard title="Turquia y Jerusalen"></PopCard>
+                             <v-btn
+                                icon
+                                @click="show = !show"
+                                >
+                                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                             </v-btn>
+                             <div>
+                             </div>
+                          </v-card-actions>
+                          <v-expand-transition>
+                             <div v-show="show">
+                                <v-divider></v-divider>
+                                <v-card-text>
+                                   <p>DÍA 01 MÉXICO ✈ ESTAMBUL</p>
+                                   <p>DÍA 02 ESTAMBUL</p>
+                                   <p>DÍA 03 ESTAMBUL</p>
+                                   <p>DÍA 04 ESTAMBUL </p>
+                                   <p>DÍA 05 ESTAMBUL 🚌 ANKARA 🚌 CAPADOCIA</p>
+                                   <p>DÍA 06 CAPADOCIA</p>
+                                   <p>DÍA 07 CAPADOCIA</p>
+                                   <p>DÍA 08 CAPADOCIA 🚌 PAMUKKALE</p>
+                                   <p>DÍA 09 PAMUKKALE 🚌 EFESO 🚌 KUSADASI*</p>
+                                   <p>DÍA 10 KUSADASI*</p>
+                                </v-card-text>
+                             </div>
+                          </v-expand-transition>
+                       </v-card>
+                    </v-slide-item>
+                    <!--//////////////////////-->
+                    <v-slide-item class="ma-5">
+                       <v-card
+                          class="mx-auto"
+                          max-width="500"
+                          >
+                          <v-img
+                             src="https://one.cdnmega.com/images/destinos/promos/europa/gran-tour-de-europa-790x1042_638559b34ddef.webp?width=290"
+                             width="500"
+                             height="450"
+                             ></v-img>
+                          <v-card-title>
+                             Europa
+                          </v-card-title>
+                          <v-card-subtitle>
+                             19 dias 17 noches
+                          </v-card-subtitle>
+                          <v-card-actions>
+                             <v-btn
+                                color="orange lighten-2"
+                                text
+                                >
+                                Informacion
+                             </v-btn>
+                             <v-spacer></v-spacer>
+                             <v-btn
+                                icon
+                                @click="see = !see"
+                                >
+                                <v-icon>{{ see ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                             </v-btn>
+                          </v-card-actions>
+                          <v-expand-transition>
+                             <div v-show="see">
+                                <v-divider></v-divider>
+                                <v-card-text>
+                                   <p>DÍA 01 MÉXICO – LONDRES</p>
+                                   <p>DÍA 02 LONDRES</p>
+                                   <p>DÍA 04 LONDRES – PARÍS </p>
+                                   <p>DÍA 05 PARÍS</p>
+                                   <p>DÍA 07 PARÍS – LUXEMBURGO – FRANKFURT </p>
+                                   <p>DÍA 08 FRANKFURT – HEIDELBERG – ZÚRICH </p>
+                                   <p>DÍA 09 ZÚRICH – LUCERNA – VADUZ – INNSBRUCK </p>
+                                   <p>DÍA 10 INNSBRUCK – PADUA – VENECIA </p>
+                                   <p>DÍA 11 VENECIA – ROMA </p>
+                                   <p>DÍA 14 ROMA – FLORENCIA </p>
+                                   <p>DÍA 15 FLORENCIA – PISA – NIZA </p>
+                                   <p>DÍA 16 NIZA – BARCELONA</p>
+                                   <p>DÍA 17 BARCELONA – ZARAGOZA – MADRID  </p>
+                                   <p>DÍA 18 MADRID  </p>
+                                   <p>DÍA 19 MADRID – MÉXICO</p>
+                                </v-card-text>
+                             </div>
+                          </v-expand-transition>
+                       </v-card>
+                    </v-slide-item>
+                    <!--/////////////////////////////-->
+                    <v-slide-item class="ma-5">
+                       <v-card
+                          class="mx-auto"
+                          max-width="500"
+                          >
+                          <v-img
+                             src="https://one.cdnmega.com/images/viajes/covers/escapate-a-chetumal-1024x575_6320bfe1dfa25.webp"
+                             width="575"
+                             height="450"
+                             ></v-img>
+                          <v-card-title>
+                             Escapate a Chetumal
+                          </v-card-title>
+                          <v-card-subtitle>
+                             4 dias / 3 noches
+                          </v-card-subtitle>
+                          <v-card-actions>
+                             <v-btn
+                                color="orange lighten-2"
+                                text
+                                >
+                                Informacion
+                             </v-btn>
+                             <v-spacer></v-spacer>
+                             <v-btn
+                                icon
+                                @click="show1 = !show1"
+                                >
+                                <v-icon>{{ show1 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                             </v-btn>
+                          </v-card-actions>
+                          <v-expand-transition>
+                             <div v-show="show1">
+                                <v-divider></v-divider>
+                                <v-card-text>
+                                   <p>DÍA 01 MÉXICO – CHETUMAL</p>
+                                   <p>DÍA 02 - 03 CHETUMAL </p>
+                                   <p>DÍA 04 CHETUMAL – MÉXICO</p>
+                                </v-card-text>
+                             </div>
+                          </v-expand-transition>
+                       </v-card>
+                    </v-slide-item>
+                    <!--/////////////////////////////-->
+                    <v-slide-item class="ma-5">
+                       <v-card class="mx-auto" max-width="500">
+                          <v-img
+                             src="https://one.cdnmega.com/images/destinos/promos/asia/turquia-y-corea-790x1042_637fb0470b0b7.webp?width=290"
+                             width="500"
+                             height="500"
+                             ></v-img>
+                          <v-card-title>
+                             Turquia y Chetumal
+                          </v-card-title>
+                          <v-card-subtitle>
+                             15 dias / 10 noches
+                          </v-card-subtitle>
+                          <v-card-actions>
+                             <v-btn color="orange lighten-2" text>
+                                Informacion
+                             </v-btn>
+                             <v-spacer></v-spacer>
+                             <v-btn icon @click="show2 = !show2">
+                                <v-icon>{{ show2 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                             </v-btn>
+                          </v-card-actions>
+                          <v-expand-transition>
+                             <div v-show="show2">
+                                <v-divider></v-divider>
+                                <v-card-text>
+                                   <p>DÍA 01 MÉXICO – CHETUMAL</p>
+                                   <p>DÍA 02 -</p>
+                                   <p>03 CHETUMAL </p>
+                                   <p>DÍA 04 CHETUMAL – MÉXICO</p>
+                                </v-card-text>
+                             </div>
+                          </v-expand-transition>
+                       </v-card>
+                    </v-slide-item>
+                 </v-slide-group>
+              </v-sheet>
+           </v-container>
+        </v-content>
+     </v-app>
+  </div>
 </template>
 <script>
-  export default {
+import PopCard from './PopCard.vue';  
+export default {
+  components: {PopCard},
     data: () => ({
       model: null,
       show:false,
       see:false,
       show1:false,
       show2:false
-    }),
+    })
   }
 </script>
---
+++ src/components/PopCard.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+        <template v-slot:activator="{ on }">
+          <v-btn
+            color="orange lighten-2"
+            text v-on="on"> 
+            Informacion
+          </v-btn>
+        </template>
+        <v-card><!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+          <v-toolbar dark color="primary">
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>X</v-icon>
+            </v-btn>
+            <v-toolbar-title>{{this.title}}</v-toolbar-title>
+            
+          </v-toolbar>
+          <v-list three-line subheader>
+            <v-subheader>Descripcion</v-subheader>
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title><p>Salida para contemplar el espectacular panorama del Cuerno de Oro desde la colina de Pierre Loti; visita de la Catedral de San Jorge, principal patriarcado de la Iglesia Ortodoxa Griega y sede del Patriarcado Ecuménico de Constantinopla, reconocido como el líder espiritual de los cristianos ortodoxos del mundo; continuamos a la Mezquita de Solimán El Magnífico, diseñada por el arquitecto otomano Mimar Sinan y que cuenta con la cúpula más grande de todas las mezquitas en Estambul; nos dirigimos al Bazar de las Especias, un lugar con encanto especial por su colorido y aromas, el sitio por excelencia para adquirir tés, hierbas, frutos secos, dulces típicos y por supuesto especias; culminamos con un recorrido a través del Bósforo, el estrecho que divide la ciudad entre Europa y Asia, apreciaremos las maravillosas vistas de las fortalezas otomanas, palacios, villas y los puentes que conectan ambos lados de esta urbe.</p></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-divider></v-divider>
+          <v-header><h4 class="title-blue">Detalles</h4></v-header>
+            <v-container class="white lighten-5">
+             <v-row>
+              <v-col>
+                <v-header class="title-blue">Incluye</v-header>
+                <ul>
+                  <li><p>Vuelo Redondo MEX-LMM CUU-MEX en clase turista</p></li>
+                  <li><p>Todos los traslados de acuerdo con el itinerario</p></li>
+                  <li><p>01 noche en El Fuerte, 01 Barrancas, 01 Creel y 01 Chihuahua</p></li>
+                  <li><p>Tour a bahía a Topolobampo en lancha en servicio compartido</p></li>
+                  <li><p>Ferrocarril CHEPE EXPRESS clase Turista, El Fuerte-Barrancas del Cobre</p></li>
+                  <li><p>Cena y desayuno en Barrancas del Cobre</p></li>
+                  <li><p>Traslado por carretera de Barrancas del Cobre - Creel-Aeropuerto de Chihuahua</p></li>
+                  <li><p>Tour Valle de los Hongos y de las Ranas Tour Campos Menonitas City tour en Chihuahua</p></li>
+                  <li><p>Desayuno diario (excepto el de llegada)</p></li>
+                </ul>
+                <v-header class="title-blue">No incluye</v-header>
+                <ul>
+                  <li><p>GASTOS PERSONALES</p></li>
+                  <li><p>PROPINAS</p></li>
+                  <li><p>NINGÚN SERVICIO NO ESPECÍFICADO</p></li>
+                  <li><p>Impuestos aéreos por persona</p></li>
+                </ul>
+              </v-col>
+              <v-col>
+                <v-carousel v-model="model">
+                  <v-carousel-item v-for="(color, i) in colors" :key="color">
+                    <v-sheet :color="color" height="100%" tile>
+                      <v-row class="fill-height" align="center" justify="center">
+                        <div class="text-h2">
+                        Slide {{ i + 1 }}
+                        </div>
+                      </v-row>
+                    </v-sheet>
+                 </v-carousel-item>
+                </v-carousel>
+              </v-col>
+            </v-row>  
+            </v-container>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+</template>
+<script>
+export default({
+  props:['title'],
+  data () {
+    return {
+      dialog: false,
+      notifications: false,
+      sound: true,
+      widgets: false
+    }
+  },
+});
+
+</script>
